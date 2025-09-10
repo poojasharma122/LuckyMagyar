b2a7dbac7d62b10a5c38877dd13377b996452f04
Rahul <rahulkumar.avology@gmail.com>
2025-09-10 18:43:53 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+ // Header Scroll JS Start
+ $(document).ready(function () {
+    $(window).scroll(function () {
+      var header = $("header");
+      header.toggleClass("fixed_header", $(window).scrollTop() > 0);
+    });
+  });
+  // Header Scroll JS End
+
+  // Toggle Menu Functionaliy Start
+$(document).ready(function () {
+  $(".menu-toggle-btn").click(function () {
+    $("body").addClass("menuToggle");
+  });
+  $(".menu-close-btn").click(function () {
+    $("body").removeClass("menuToggle");
+  });
+});
+// Toggle Menu Functionaliy End
